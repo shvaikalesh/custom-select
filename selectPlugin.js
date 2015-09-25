d32f8393c22cc4d689a4845e3b950bdd1536948f
Stas Gavrylov <stasgavrylov@gmail.com>
2015-09-25 14:03:42 +0300

    Saving progress.

--- selectPlugin.js
+++ selectPlugin.js
@@ -1,24 +1,47 @@
 !function() {
+
     // Init plugin.
     var selectElements = $$('select:not([mutiple])');
         selectElements.forEach(replaceSelectElement);
 
     // Replace <select> with custom structure.
     function replaceSelectElement(select) {
+        // Helpers.
+        function getOptFromDiv(div) {
+            return customOptionsArray.indexOf(div);
+        }
+        function getDivFromOpt(opt) {
+            return optionsArray.indexOf(opt);
+        }
+
         // TODO.
         var customSelect = $('<div>');
         customSelect.classList.add('custom-select');
-        // customSelect.dataset.name = select.name;
-        // customSelect.dataset.required = select.required;
+
+        // Arrays to store options and customOptions (divs).
+        var optionsArray = [], customOptionsArray = [];
+
+
+
+        // Dealing with styles.
+        var originStyles = getComputedStyle(select); // default <select> styles.
+        var userStyles = {select: {},
+                          option: {
+                                    backgroundColorHover: 'lightblue',
+                                    backgroundColor: 'white'
+                                  }}; // mock object for styles set by user.
+
+
+
+
 
         // Transfer browser styles from <select> to customSelect.
-        function transferStyles(element, origin, props) {
-            var elementsStyles = getComputedStyle(origin);
+        function transferStyles(element, props, stylesObject) {
             props.forEach(function(prop) {
-                element.style[prop] = elementsStyles[prop];
+                element.style[prop] = stylesObject[prop];
             });
         }
-        transferStyles(customSelect, select, ['border', 'display', 'height', 'font']);
+        transferStyles(customSelect, ['border', 'display', 'height', 'width', 'font'], originStyles);
         customSelect.style.overflow = 'hidden';
 
         // Selected <option>.
@@ -28,26 +51,62 @@
         customSelect.append(selectedOption);
 
 
-        // Hidden <option>s.
+        // Fill hidden list, fill arrays of options and customOptions.
         var innerList = $('<div>');
+        transferStyles(innerList, ['border'], originStyles);
         customSelect.append(innerList);
         var opts = select.queryAll('option, optgroup');
 
-        opts.forEach(function(option) {
+        innerList.append.apply(innerList, opts.map(option => {
             var customOption = $('<div>');
-            customOption.textContent = option.textContent;
-            innerList.append(customOption);
+                customOption.textContent = option.textContent;
+
+            if (option.tagName == 'OPTION') {
+                optionsArray.push(option);
+                customOptionsArray.push(customOption);
+            }
+            return customOption;
+        }));
+
+
+
+
+
+        // Assign handlers.
+        // Open/close customSelect.
+        $.on(customSelect, 'click', () => {
+            if (customSelect.style.overflow == 'hidden') {
+                customSelect.style.overflow = 'visible';
+            } else {
+                customSelect.style.overflow = 'hidden';
+            }
         });
 
-        innerList.append.apply(innerList, opts.map(option =>
-        {
-            var customOption = $('<div>')
-                customOption.textContent = option.textContent
+        // Detect list hover.
+        $.on(innerList, 'mouseover', (e) => {
+            if (e.target == innerList) return;
+            else
+                e.target.style.backgroundColor = userStyles.option.backgroundColorHover;
+        })
+        $.on(innerList, 'mouseout', (e) => {
+            if (e.target == innerList) return;
+            else
+                e.target.style.backgroundColor = userStyles.option.backgroundColor;
+        })
+        $.on(innerList, 'click', (e) => {
+            if (e.target == innerList) return;
+            else
+                // console.log(e.target);
+            selectedOption.textContent = e.target.textContent;
+            select.selectedIndex = opts.indexOf(e.target.textContent);
+            console.log(opts, e.target, opts.indexOf(e.target));
 
-            return customOption
-        }))
+        })
+
+
 
         document.body.append(customSelect);
+    console.log(optionsArray, customOptionsArray);
     } // replaceSelectElement.
 
     // a = $('select');
