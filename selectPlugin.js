7986a731ee2e28f99040cf06be557aaf36d4dc43
Stas Gavrylov <stasgavrylov@gmail.com>
2015-09-25 17:50:41 +0300

    Saving progress.

--- selectPlugin.js
+++ selectPlugin.js
@@ -3,6 +3,8 @@
     // Init plugin.
     var selectElements = $$('select:not([mutiple])');
         selectElements.forEach(replaceSelectElement);
+    // Var to store current opened customSelect.
+    var currentOpenedCustomSelect;
 
     // Replace <select> with custom structure.
     function replaceSelectElement(select) {
@@ -13,26 +15,39 @@
         function getDivFromOpt(opt) {
             return optionsArray.indexOf(opt);
         }
+        var optionsArray = [], customOptionsArray = []; // Arrays to store options and customOptions (divs).
 
-        // TODO.
+        // Deal with styles.
+        var originStyles = getComputedStyle(select); // default <select> styles.
+        var userStyles = {select:   {},
+                          option:   {
+                                      backgroundColorHover: 'lightblue',
+                                      backgroundColor: 'white',
+                                      paddingInsideOptgroup: '10px'
+                                    },
+                          optgroup: {
+                                        fontWeight: 'bold',
+                                        paddingLeft: '5px'
+                                    }}; // mock object for styles set by user.
+
+
+
+
+
+        // Create wrapper.
+        var customParent = $('<div>');
+        select.before(customParent);
+        customParent.append(select);
+        customParent.style.fontSize = '0px';
+
+        
+
+
+        /*
+         *  CUSTOM SELECT.
+         */
         var customSelect = $('<div>');
         customSelect.classList.add('custom-select');
-
-        // Arrays to store options and customOptions (divs).
-        var optionsArray = [], customOptionsArray = [];
-
-
-
-        // Dealing with styles.
-        var originStyles = getComputedStyle(select); // default <select> styles.
-        var userStyles = {select: {},
-                          option: {
-                                    backgroundColorHover: 'lightblue',
-                                    backgroundColor: 'white'
-                                  }}; // mock object for styles set by user.
-
-
-
 
 
         // Transfer browser styles from <select> to customSelect.
@@ -41,31 +56,46 @@
                 element.style[prop] = stylesObject[prop];
             });
         }
-        transferStyles(customSelect, ['border', 'display', 'height', 'width', 'font'], originStyles);
+        transferStyles(customSelect, ['display', 'height', 'width', 'font', 'box-sizing'], originStyles);
         customSelect.style.overflow = 'hidden';
+        customSelect.style.position = 'relative';
+        customSelect.style.border = '3px solid red';
 
         // Selected <option>.
-        var selectedOption = $('<div>');
-        selectedOption.textContent = select[select.selectedIndex].textContent;
-        selectedOption.style.height = '100%';
-        customSelect.append(selectedOption);
+        var customSelectedOption = $('<div>');
+        customSelectedOption.textContent = select[select.selectedIndex].textContent;
+        customSelectedOption.style.height = '100%';
+        customSelect.append(customSelectedOption);
 
 
         // Fill hidden list, fill arrays of options and customOptions.
         var innerList = $('<div>');
-        transferStyles(innerList, ['border'], originStyles);
+        transferStyles(innerList, ['border', 'padding', 'width', 'box-sizing'], originStyles);
+        innerList.style.position = 'absolute';
+        innerList.style.zIndex = '1';
         customSelect.append(innerList);
         var opts = select.queryAll('option, optgroup');
 
         innerList.append.apply(innerList, opts.map(option => {
             var customOption = $('<div>');
-                customOption.textContent = option.textContent;
+                customOption.style.backgroundColor = userStyles.option.backgroundColor;
 
             if (option.tagName == 'OPTION') {
+                customOption.textContent = option.textContent;
+                customOption.dataset.tag = 'option';
+
                 optionsArray.push(option);
                 customOptionsArray.push(customOption);
+                if (option.parentNode.tagName == 'OPTGROUP')
+                    customOption.style.paddingLeft = userStyles.option.paddingInsideOptgroup;
+            } else {
+                customOption.textContent = option.label;
+                customOption.dataset.tag = 'optgroup';
+                customOption.style.fontWeight = userStyles.optgroup.fontWeight;
+                customOption.style.paddingLeft = userStyles.optgroup.paddingLeft;
             }
             return customOption;
+
         }));
 
 
@@ -74,39 +104,44 @@
 
         // Assign handlers.
         // Open/close customSelect.
-        $.on(customSelect, 'click', () => {
+        $.on(customSelect, 'click', (e) => {
+            
             if (customSelect.style.overflow == 'hidden') {
+
+                currentOpenedCustomSelect ? currentOpenedCustomSelect.style.overflow = 'hidden' : currentOpenedCustomSelect = customSelect;
+
                 customSelect.style.overflow = 'visible';
+                currentOpenedCustomSelect = customSelect;
             } else {
                 customSelect.style.overflow = 'hidden';
             }
         });
 
-        // Detect list hover.
+        // Detect innerList events.
         $.on(innerList, 'mouseover', (e) => {
-            if (e.target == innerList) return;
+            if (e.target == innerList || e.target.dataset.tag == 'optgroup') return;
             else
                 e.target.style.backgroundColor = userStyles.option.backgroundColorHover;
         })
         $.on(innerList, 'mouseout', (e) => {
-            if (e.target == innerList) return;
+            if (e.target == innerList || e.target.dataset.tag == 'optgroup') return;
             else
                 e.target.style.backgroundColor = userStyles.option.backgroundColor;
         })
         $.on(innerList, 'click', (e) => {
-            if (e.target == innerList) return;
+            if (e.target == innerList || e.target.dataset.tag == 'optgroup') {
+                return;
+            }
             else
-                // console.log(e.target);
-            selectedOption.textContent = e.target.textContent;
-            select.selectedIndex = opts.indexOf(e.target.textContent);
-            console.log(opts, e.target, opts.indexOf(e.target));
-
+            customSelectedOption.textContent = e.target.textContent;
+            select.selectedIndex = getOptFromDiv(e.target);
         })
 
+        
 
 
-        document.body.append(customSelect);
-    console.log(optionsArray, customOptionsArray);
+
+        select.after(customSelect);
     } // replaceSelectElement.
 
     // a = $('select');
@@ -114,4 +149,12 @@
     // console.log(getComputedStyle(a));
     // console.log(getComputedStyle(b));
 
+
+
+    // Detect clicks outside of innerList.
+    $.on(document.body, 'click', (e) => {
+        var cS = e.target.closest('.custom-select');
+        if (!cS)
+            $$('.custom-select').forEach(el => {el.style.overflow = 'hidden'});
+    })
 }()
