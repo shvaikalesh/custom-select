4ec1a31a8bf73f08af1150024a50c8affba60018
Stas Gavrylov <stasgavrylov@gmail.com>
2015-09-28 15:18:26 +0300

    Saving progress.

--- selectPlugin.js
+++ selectPlugin.js
@@ -1,147 +1,135 @@
-!function() {
+!(function() {
+    // Helpers.
+    function getOptFromDiv(div) {
+        return customOptionsArray.indexOf(div);
+    }
+    function getDivFromOpt(opt) {
+        return optionsArray.indexOf(opt);
+    }
+    function createPrefixedElement(prefix) {
+        return function(el, className) {
+            var customEl = $('<' + el + '>');
+            customEl.classList.add(prefix + '-' + className);
+            return customEl;
+        }
+    }
+    var createHRselectBlock = createPrefixedElement('hr-select');
+
+
+
+
 
     // Init plugin.
     var selectElements = $$('select:not([mutiple])');
         selectElements.forEach(replaceSelectElement);
-    // Var to store current opened customSelect.
+    // Var to store current opened HRSelect.
     var currentOpenedCustomSelect;
 
     // Replace <select> with custom structure.
     function replaceSelectElement(select) {
-        // Helpers.
-        function getOptFromDiv(div) {
-            return customOptionsArray.indexOf(div);
-        }
-        function getDivFromOpt(opt) {
-            return optionsArray.indexOf(opt);
-        }
-        var optionsArray = [], customOptionsArray = []; // Arrays to store options and customOptions (divs).
-
-        // Deal with styles.
-        var originStyles = getComputedStyle(select); // default <select> styles.
-        var userStyles = {select:   {},
-                          option:   {
-                                      backgroundColorHover: 'lightblue',
-                                      backgroundColor: 'white',
-                                      paddingInsideOptgroup: '10px'
-                                    },
-                          optgroup: {
-                                        fontWeight: 'bold',
-                                        paddingLeft: '5px'
-                                    }}; // mock object for styles set by user.
-
-
-
-
-
-        // Create wrapper.
-        var customParent = $('<div>');
-        select.before(customParent);
-        customParent.append(select);
-        customParent.style.fontSize = '0px';
-
-        
-
 
         /*
          *  CUSTOM SELECT.
          */
-        var customSelect = $('<div>');
-        customSelect.classList.add('custom-select');
+        var HRSelect = createHRselectBlock('div', 'wrapper');
+        select.before(HRSelect);
 
-
-        // Transfer browser styles from <select> to customSelect.
-        function transferStyles(element, props, stylesObject) {
-            props.forEach(function(prop) {
-                element.style[prop] = stylesObject[prop];
-            });
-        }
-        transferStyles(customSelect, ['display', 'height', 'width', 'font', 'box-sizing'], originStyles);
-        customSelect.style.overflow = 'hidden';
-        customSelect.style.position = 'relative';
-        customSelect.style.border = '3px solid red';
-
-        // Selected <option>.
-        var customSelectedOption = $('<div>');
-        customSelectedOption.textContent = select[select.selectedIndex].textContent;
-        customSelectedOption.style.height = '100%';
-        customSelect.append(customSelectedOption);
-
-
-        // Fill hidden list, fill arrays of options and customOptions.
-        var innerList = $('<div>');
-        transferStyles(innerList, ['border', 'padding', 'width', 'box-sizing'], originStyles);
-        innerList.style.position = 'absolute';
-        innerList.style.zIndex = '1';
-        customSelect.append(innerList);
-        var opts = select.queryAll('option, optgroup');
-
-        innerList.append.apply(innerList, opts.map(option => {
-            var customOption = $('<div>');
-                customOption.style.backgroundColor = userStyles.option.backgroundColor;
-
-            if (option.tagName == 'OPTION') {
-                customOption.textContent = option.textContent;
-                customOption.dataset.tag = 'option';
-
-                optionsArray.push(option);
-                customOptionsArray.push(customOption);
-                if (option.parentNode.tagName == 'OPTGROUP')
-                    customOption.style.paddingLeft = userStyles.option.paddingInsideOptgroup;
-            } else {
-                customOption.textContent = option.label;
-                customOption.dataset.tag = 'optgroup';
-                customOption.style.fontWeight = userStyles.optgroup.fontWeight;
-                customOption.style.paddingLeft = userStyles.optgroup.paddingLeft;
+        // Blocks.
+        function addHRPrefixedBlock(parent) {
+            return function(type, className) {
+                var block = createHRselectBlock(type, className);
+                parent.append(block);
+                return block;
             }
-            return customOption;
+        }
+        var addBlockToHRSelect = addHRPrefixedBlock(HRSelect);
 
-        }));
+        var HRSelectSearch = addBlockToHRSelect('input', 'search');
+            HRSelectSearch.type = 'search';
 
+        var HRSelectBefore = addBlockToHRSelect('div', 'before');
 
+        var HRSelectCurrent = addBlockToHRSelect('div', 'current');
+            HRSelectCurrent.textContent = select.options[select.selectedIndex].textContent;
 
+        var HRSelectAfter = addBlockToHRSelect('div', 'after');
+
+        var HRSelectList = addBlockToHRSelect('ul', 'list');
+        HRSelect.append(select);
+
+        // Transfer select children.
+        function transferChildren(parent) {
+
+            return Array.prototype.map.call(parent.children, function(child) {
+                var newChild = $('<li>');
+                var tag = child.tagName.toLowerCase();
+
+                switch (tag)
+                {
+                    case 'option':
+                        newChild.textContent = child.textContent;
+                        break;
+
+                    case 'optgroup':
+                        newChild.classList.add('hr-select-group');
+                        var label = createHRselectBlock('div', 'label');
+                        label.textContent = child.label;
+                        newChild.append(label);
+                        newChild.append.apply(newChild, transferChildren(child));
+                        break;
+                }
+                return newChild;
+            });
+
+        }
+        HRSelectList.append.apply(HRSelectList, transferChildren(select));
 
 
         // Assign handlers.
-        // Open/close customSelect.
-        $.on(customSelect, 'click', (e) => {
-            
-            if (customSelect.style.overflow == 'hidden') {
+        // Open/close HRSelect.
+        $.on(HRSelectCurrent, 'click', (e) => {
+            console.log(HRSelectList.style.display);
+            if (HRSelectList.style.display == 'none') {
+                if (currentOpenedCustomSelect) {
+                    currentOpenedCustomSelect.style.display = 'none';
+                }
 
-                currentOpenedCustomSelect ? currentOpenedCustomSelect.style.overflow = 'hidden' : currentOpenedCustomSelect = customSelect;
-
-                customSelect.style.overflow = 'visible';
-                currentOpenedCustomSelect = customSelect;
+                HRSelectList.style.display = 'block';
+                console.log(HRSelectList);
+                currentOpenedCustomSelect = HRSelectList;
+                // console.log(currentOpenedCustomSelect);
             } else {
-                customSelect.style.overflow = 'hidden';
+                HRSelectList.style.display = 'none';
             }
         });
 
-        // Detect innerList events.
-        $.on(innerList, 'mouseover', (e) => {
-            if (e.target == innerList || e.target.dataset.tag == 'optgroup') return;
+        // Detect HRSelectList events.
+        /*$.on(HRSelectList, 'mouseover', (e) => {
+            if (e.target == HRSelectList || e.target.dataset.tag == 'optgroup') return;
             else
                 e.target.style.backgroundColor = userStyles.option.backgroundColorHover;
         })
-        $.on(innerList, 'mouseout', (e) => {
-            if (e.target == innerList || e.target.dataset.tag == 'optgroup') return;
+        $.on(HRSelectList, 'mouseout', (e) => {
+            if (e.target == HRSelectList || e.target.dataset.tag == 'optgroup') return;
             else
                 e.target.style.backgroundColor = userStyles.option.backgroundColor;
         })
-        $.on(innerList, 'click', (e) => {
-            if (e.target == innerList || e.target.dataset.tag == 'optgroup') {
+        $.on(HRSelectList, 'click', (e) => {
+            if (e.target == HRSelectList || e.target.dataset.tag == 'optgroup') {
                 return;
             }
             else
-            customSelectedOption.textContent = e.target.textContent;
+            HRSelectedOption.textContent = e.target.textContent;
             select.selectedIndex = getOptFromDiv(e.target);
-        })
+        })*/
 
         
 
+        // DECOR.
+        select.style.position = 'relative';
+        select.style.left = '200px';
 
 
-        select.after(customSelect);
     } // replaceSelectElement.
 
     // a = $('select');
@@ -151,10 +139,11 @@
 
 
 
-    // Detect clicks outside of innerList.
-    $.on(document.body, 'click', (e) => {
+    // Detect clicks outside of HRSelectList.
+    $.on(document, 'click', (e) => {
         var cS = e.target.closest('.custom-select');
-        if (!cS)
-            $$('.custom-select').forEach(el => {el.style.overflow = 'hidden'});
+        if (!cS && currentOpenedCustomSelect) {
+            currentOpenedCustomSelect.style.display = 'none';
+        }
     })
-}()
+})()
